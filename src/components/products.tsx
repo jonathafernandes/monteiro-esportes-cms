c87fba6cbb7419e4cd530f90780ccb180e47b9de
jonathafernandes <jonathafernandes@live.com>
2024-05-18 12:03:09 -0300

    feat: create modal images

--- src/components/products.tsx
+++ src/components/products.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { client } from '../services/prismic';
 import { Minus, WhatsappLogo } from '@phosphor-icons/react';
+import * as Dialog from '@radix-ui/react-dialog';
 
 interface Product {
     id: string;
@@ -42,31 +43,38 @@ export function Products() {
     }, []);
 
     return (
-        <div id="products"className="w-full flex flex-col">
+        <div id="products" className="w-full flex flex-col">
             <h3 className="text-2xl mt-24 font-bold sm:text-start text-center sm:ml-10">Produtos</h3>
             <Minus size={32} className="sm:ml-10 my-0 mx-auto" />
             <div className="flex flex-wrap mt-12 mb-32 sm:ml-10 gap-4 font-mono sm:justify-start justify-center">
                 {products.length === 0 && <h2 className='text-xl'>Carregando produtos...</h2>}
                 {products.map(product => (
-                    <div key={product.id} className="w-[80vw] sm:w-56 flex flex-col p-4 rounded-md bg-gray-100 shadow-md ring-1 ring-gray-200 transition-transform transform hover:-translate-y-2">
-                        {product.data.imagem.url === undefined && <h2 className='text-xs mb-2'>Imagem não disponível</h2>}
-                        <img
-                            src={product.data.imagem.url}
-                            alt={product.data.nome[0].text}
-                            className='w-[80vw]'
-                        />
-                        <h4 className="text-sm mt-2 text-transform: uppercase">{product.data.nome[0].text}</h4>
-                        <p className="text-lg font-bold">R$ {product.data.preco[0].text}</p>
-                        <p className="text-xs">Até 8x sem juros</p>
-                        <button className="bg-[#25d366] border border-[#128c7e] mt-2 p-1 transition duration-300 hover:bg-[#1ebd59] text-xs">
-                            <a href="https://api.whatsapp.com/send/?phone=82988860744&text&type=phone_number&app_absent=0" target="_blank" className="flex items-center gap-1 justify-center">
-                                <span>
-                                Compre pelo WhatsApp
-                                </span>
-                                <WhatsappLogo size={18} weight="fill" />
-                            </a>
-                        </button>
-                    </div>
+                    <Dialog.Root key={product.id}>
+                        <Dialog.Trigger className="w-[80vw] sm:w-56 flex flex-col p-4 rounded-md bg-gray-100 shadow-md ring-1 ring-gray-200 transition-transform transform hover:-translate-y-2">
+                            {product.data.imagem.url === undefined && <h2 className='text-xs mb-2'>Imagem não disponível</h2>}
+                            <img
+                                src={product.data.imagem.url}
+                                alt={product.data.nome[0].text}
+                                className='w-full h-auto object-cover'
+                            />
+                            <h4 className="text-sm mt-2 text-transform: uppercase">{product.data.nome[0].text}</h4>
+                            <p className="text-lg font-bold">R$ {product.data.preco[0].text}</p>
+                            <p className="text-xs">Até 8x sem juros</p>
+                            <button className="bg-[#25d366] border border-[#128c7e] mt-2 p-1 transition duration-300 hover:bg-[#1ebd59] text-xs">
+                                <a href="https://api.whatsapp.com/send/?phone=82988860744&text&type=phone_number&app_absent=0" target="_blank" className="flex items-center gap-1 justify-center">
+                                    <span>Compre pelo WhatsApp</span>
+                                    <WhatsappLogo size={18} weight="fill" />
+                                </a>
+                            </button>
+                        </Dialog.Trigger>
+
+                        <Dialog.Portal>
+                            <Dialog.Overlay className="fixed inset-0 bg-black/50" />
+                            <Dialog.Content className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[440px] w-[97%] sm:w-full bg-white rounded-md outline-none">
+                                <img src={product.data.imagem.url} alt={product.data.nome[0].text} className="w-full h-auto object-cover rounded-md" />
+                            </Dialog.Content>
+                        </Dialog.Portal>
+                    </Dialog.Root>
                 ))}
             </div>
         </div>
