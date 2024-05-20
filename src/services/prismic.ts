ca823db867cbf4f2e89c7e6b485a206a84e67777
jonathafernandes <jonathafernandes@live.com>
2024-05-20 13:53:25 -0300

    chore: add environment variables

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,7 +1,6 @@
 import * as Prismic from '@prismicio/client';
 
-export const client = Prismic.createClient('monteiro-esportes', {
-    accessToken: 
-        'MC5aa1luR1JBQUFDQUFGY2Nn.Ahvvv71N77-977-9Ke-_ve-_vWvvv70U77-977-9H--_vWhJNVrvv73vv71vRGPvv702Q--_vWDvv73vv70'
+export const client = Prismic.createClient(`${import.meta.env.VITE_PRISMIC_ENVIRONMENT}`, {
+    accessToken: `${import.meta.env.VITE_PRISMIC_ACCESS_TOKEN}`,
     }
 );
